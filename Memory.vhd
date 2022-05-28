413ea151748925c1cf31cb21005f26f8f05ea7d5
moza-loza <61754531+MoazTarekAli@users.noreply.github.com>
2022-05-28 02:00:09 +0200

    Modified memory to be more stable

--- Memory.vhd
+++ Memory.vhd
@@ -7,6 +7,7 @@ ENTITY Memory IS
 		clk         : IN std_logic;
 		WriteEnable : IN std_logic;
 		ReadEnable  : IN std_logic;
+		Reset       : IN std_logic; 
 		address     : IN  std_logic_vector(19 DOWNTO 0);
         ProgramCounter : IN  std_logic_vector(19 DOWNTO 0);
 		datain      : IN  std_logic_vector(31 DOWNTO 0);
@@ -19,27 +20,27 @@ ARCHITECTURE syncrama OF Memory IS
 	SIGNAL ram : ram_type;
 	
 	BEGIN
-		PROCESS(clk) IS
+		PROCESS(clk, Reset) IS
 			BEGIN
-				IF rising_edge(clk) THEN
-                    -- checking for write
+				-- checking for reset to get M[0]
+				IF (Reset = '1') THEN
+					Intruction <= ram(0);
+					dataout <= (others => 'Z');
+				ELSIF rising_edge(clk) THEN			
+					-- checking for write
 					IF WriteEnable = '1' THEN
 						ram(to_integer(unsigned(address))) <= datain;
-					END IF;
-
-                    -- checking for read
-					IF ReadEnable = '1' THEN
-						dataout <= ram(to_integer(unsigned(address)));
-					ELSE
-						dataout <= (others => 'Z');
-					END IF;
-
-                    -- checking for instruction
-                    IF ReadEnable = '0' and WriteEnable = '0' THEN
-						Intruction <= ram(to_integer(unsigned(ProgramCounter)));
-					ELSE
 						Intruction <= (others => 'Z');
-					END IF;                    
+						dataout <= (others => 'Z');
+						-- checking for read
+					ELSIF ReadEnable = '1' THEN
+						dataout <= ram(to_integer(unsigned(address)));
+						Intruction <= (others => 'Z');
+					-- default taking PC
+					ELSE
+						Intruction <= ram(to_integer(unsigned(ProgramCounter)));
+						dataout <= (others => 'Z');
+					END IF;               
 				END IF;
 		END PROCESS;
 END syncrama;
